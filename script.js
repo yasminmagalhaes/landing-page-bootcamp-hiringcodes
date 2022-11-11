b2884587e86039c9f46397059a3cfdf1dddd51c3
Yasmin Magalhaes <yasmindemagalhaes1@gmail.com>
2022-11-11 02:53:23 -0300

    Separando o Script

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function displayAbout() {
+    var arrow = document.getElementById('animation');
+    arrow.classList.toggle('rotate');
+    var display = document. getElementById('about');
+    display.classList.toggle('active');
+}
